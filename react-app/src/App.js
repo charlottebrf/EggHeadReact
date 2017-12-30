47ea051878de0d6344c097df8bab87a06dcdb02f
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-30 11:41:07 +0000

    [Charlotte] Uses render function to display Hello World in App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const App = () => <h1>Hello</h1>
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <b>Bold</b>
+            </div>
+            )
+    }
+}
+
 
 export default App
