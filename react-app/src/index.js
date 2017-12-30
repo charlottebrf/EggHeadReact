3b15ec27babf6ffcbb4702d26589fd8a4b7836a9
Charlotte Fereday <cfereday@thoughtworks.com>
2017-12-30 12:27:22 +0000

    [Charlotte] Adds in txt & cat properties to App

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App cat={5} txt="this is the prop text"/>, document.getElementById('root'));
 registerServiceWorker();
